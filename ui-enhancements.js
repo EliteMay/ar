a4605dc16542baba3d76883dfa1b010645958f1b
EliteMay <k12m45k@gmail.com>
2026-08-29 00:42:38 +0900

    Bump ASMRTube UI version to v1.5

--- ui-enhancements.js
+++ ui-enhancements.js
@@ -1,4 +1,4 @@
-// ASMRTube product UI enhancements v1.4
+// ASMRTube product UI enhancements v1.5
 (function(){
   const SETTINGS_KEY='asmrtube.settings.v1';
   const $=s=>document.querySelector(s);
@@ -74,7 +74,7 @@
   if(sidebar){
     const version=document.createElement('div');
     version.className='sidebar-version';
-    version.innerHTML='<span>ASMRTUBE</span><strong>v1.4</strong>';
+    version.innerHTML='<span>ASMRTUBE</span><strong>v1.5</strong>';
     sidebar.appendChild(version);
   }
 
